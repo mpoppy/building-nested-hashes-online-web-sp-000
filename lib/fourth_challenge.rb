94cf347b47c39f6362a43855b11ff07b556f377d
Marc Poppy <poppy.marc@gmail.com>
2019-05-21 04:33:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,12 +6,16 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-        :name => "",
-        :age => "",
-        :attitude => ""
-        }
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"  
+      },
+      {
+        :name => "Mercutio",
+        :age => "18"
+      }
       ]
-   },
+   }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -22,6 +26,6 @@ def fourth_challenge
    }
 }
 
-
+  
 
 end
